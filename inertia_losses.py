grad logging bug fix; minor refactors of loss computation

inertia_losses.py
@@ -248,8 +248,8 @@ class ReconLossA:
         # compute loss on root pos
         root_pos_gt = qpos_gt[:, :, :3]
         root_pos_pred = qpos_pred[:, :, :3]
-        root_pos_diff_norm = torch.norm(
-            root_pos_pred - root_pos_gt, p="fro", dim=-1).view(num_sims, num_steps)
+        root_pos_diff_norm = torch.linalg.vector_norm(
+            root_pos_pred - root_pos_gt, ord=2, dim=-1).view(num_sims, num_steps)
         root_pos_diff_sums = torch.sum(root_pos_diff_norm, dim=-1).view(num_sims,)
         loss_root_pos = torch.mean(root_pos_diff_sums)
         
@@ -267,16 +267,18 @@ class ReconLossA:
             root_rot_pred)
         root_rot_pred_mat = self.angle_util.get_44_rotation_matrix_from_33_rotation_matrix(
             root_rot_pred_mat).view(num_sims, num_steps, 4, 4)
-        root_rot_diff_norm = torch.norm(
-            root_rot_pred_mat - root_rot_gt_mat, p="fro", dim=(-2, -1)).view(num_sims, num_steps)
+        root_rot_diff_norm = torch.linalg.matrix_norm(
+            root_rot_pred_mat - root_rot_gt_mat,
+            ord="fro", dim=(-2, -1)).view(num_sims, num_steps)
         root_rot_diff_sums = torch.sum(root_rot_diff_norm, dim=-1).view(num_sims,)
         loss_root_rot = torch.mean(root_rot_diff_sums)
 
         # compute loss on joint angles
         poses_gt = qpos_gt[:, :, 6:-1].view(num_sims, num_steps, 40)
         poses_pred = qpos_pred[:, :, 6:-1].view(num_sims, num_steps, 40)
-        poss_diff_norms = torch.norm(
-            poses_pred - poses_gt, p=self.pose_loss_norm, dim=-1).view(num_sims, num_steps)
+        poss_diff_norms = torch.linalg.vector_norm(
+            poses_pred - poses_gt,
+            ord=self.pose_loss_norm, dim=-1).view(num_sims, num_steps)
         poss_diff_sums = torch.sum(poss_diff_norms, dim=-1).view(num_sims,)
         loss_poses = torch.mean(poss_diff_sums)
         
@@ -324,8 +326,9 @@ class ReconLossB:
         # sum up error norms across steps, similar to Eq. 11 from
         # "Physically Plausible Reconstruction from Monocular Videos"
         # then average across sequences
-        qpos_diff_norm = torch.norm(
-            qpos_pred - qpos_gt, p="fro", dim=-1).view(num_sims, num_steps)
+        qpos_diff_norm = torch.linalg.vector_norm(
+            qpos_pred - qpos_gt,
+            ord=2, dim=-1).view(num_sims, num_steps)
         qpos_diff_sums = torch.sum(qpos_diff_norm, dim=-1).view(num_sims,)
         loss = torch.mean(qpos_diff_sums)
         return {

inertia_trainer.py
@@ -606,28 +606,29 @@ class Trainer():
                     "train_loss/loss_root_rot", loss_dict["loss_root_rot"], train_step_idx)
                 self.writer.add_scalar(
                     "train_loss/loss_poses", loss_dict["loss_poses"], train_step_idx)
-                if len(inertia_grads) > 0:
-                    if self.predict_M_inv:
-                        scalar_name = "train_grads/M_inv_grads_norm"
-                    else:
-                        scalar_name = "train_grads/M_grads_norm"
-                    self.writer.add_scalar(
-                        scalar_name,
-                        torch.linalg.norm(inertia_grads[0]).cpu().detach().numpy(),
-                        train_step_idx)
-                for name, params_grads in weights_grads.items():
-                    grad_max = torch.max(
-                        torch.abs(params_grads)).cpu().detach().numpy()
-                    grad_mean = torch.mean(
-                        torch.abs(params_grads)).cpu().detach().numpy()
-                    self.writer.add_scalar(
-                        f"train_grads/{name}_grads_max",
-                        grad_max,
-                        train_step_idx)
-                    self.writer.add_scalar(
-                        f"train_grads/{name}_grads_mean",
-                        grad_mean,
-                        train_step_idx)
+                if self.inertia_estimator_specs["network"] != "CRBA":
+                    if len(inertia_grads) > 0:
+                        if self.predict_M_inv:
+                            scalar_name = "train_grads/M_inv_grads_norm"
+                        else:
+                            scalar_name = "train_grads/M_grads_norm"
+                        self.writer.add_scalar(
+                            scalar_name,
+                            torch.linalg.norm(inertia_grads[0]).cpu().detach().numpy(),
+                            train_step_idx)
+                    for name, params_grads in weights_grads.items():
+                        grad_max = torch.max(
+                            torch.abs(params_grads)).cpu().detach().numpy()
+                        grad_mean = torch.mean(
+                            torch.abs(params_grads)).cpu().detach().numpy()
+                        self.writer.add_scalar(
+                            f"train_grads/{name}_grads_max",
+                            grad_max,
+                            train_step_idx)
+                        self.writer.add_scalar(
+                            f"train_grads/{name}_grads_mean",
+                            grad_mean,
+                            train_step_idx)
                 
     def save_model(self):
         if self.inertia_estimator_specs['network'] != "CRBA":